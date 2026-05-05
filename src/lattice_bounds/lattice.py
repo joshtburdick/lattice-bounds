87eeb31c50c5c53574de9dcd4cabc64ba4b58fee
Josh Burdick <josh.t.burdick@gmail.com>
2026-05-05 14:46:44 -0400

    feat: add CI workflow and improve docstrings, pylint compliance, and code correctness in lattice module

--- src/lattice_bounds/lattice.py
+++ src/lattice_bounds/lattice.py
@@ -1,8 +1,13 @@
-# Defines the lattice of zeroing ("zonking") of hypergraphs.
+"""
+Defines the lattice of zeroing ("zonking") of hypergraphs.
+"""
 
 import itertools
 
 def powerset(iterable):
+    """
+    Returns the powerset of an iterable.
+    """
     s = list(iterable)
     return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
 
@@ -33,17 +38,17 @@ class CliqueLattice:
         return frozenset([c for c in self.cliques if not key.issubset(c)])
 
 
-    def zero_out_edges(self, edge):
+    def zero_out_edges(self, edge):  # pylint: disable=unused-argument
         """
         Finds the cliques remaining, after zeroing out an edge.
 
         """
         # get the vertices which are relevant
-        vertices = set(list(itertools.chain(*cliques)))
+        vertices = set(list(itertools.chain(*self.cliques)))
         # the edges, which we could zero out
         edges = itertools.combinations(vertices, 2)
         def cliques_remaining(edge):
             # note that < is comparing frozensets
-            return frozenset([c for c in cliques if not frozenset(edge) < c])
+            return frozenset([c for c in self.cliques if not frozenset(edge) < c])
         # note that this removes self-loops
-        return list(set([cliques_remaining(e) for e in edges]) - set([cliques]))
+        return list({cliques_remaining(e) for e in edges} - {self.cliques})
--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -1,12 +1,17 @@
-import pytest
+"""
+Tests for the lattice module.
+"""
 from lattice_bounds.lattice import CliqueLattice
 
 def test_hypergraph_count():
+    """
+    Verifies that the total number of hypergraphs is 2^N, where N is the number of cliques.
+    """
     n = 5
     k = 3
     lattice = CliqueLattice(n, k)
-    
+
     num_cliques = len(lattice.cliques)
     expected_hypergraph_count = 2 ** num_cliques
-    
+
     assert len(lattice.hypergraphs) == expected_hypergraph_count
